c12e5cc4bb02b67de6928b1da675addd9ae06ae2
natebouchat <natebouchat@gmail.com>
2024-02-28 23:32:36 -0600

    Created Drone Compartment item

--- ReturnsDroneRevive/ReturnsDroneRevive.cs
+++ ReturnsDroneRevive/ReturnsDroneRevive.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
+using System.Reflection;
 using BepInEx;
-using IL.RoR2;
-using IL.RoR2.UI;
+using Mono.Security.Authenticode;
 using R2API;
 using RoR2;
-using RoR2.UI;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceProviders;
 
 namespace ReturnsDroneRevive
 {
@@ -22,19 +22,45 @@ namespace ReturnsDroneRevive
 
         private static bool isTransformed = false;
         private static GameObject playerSelectedCharacterBody;
-        private static RoR2.CharacterMaster playerInstance;
+        private static CharacterMaster playerInstance;
         private List<ItemIndex> savedInventory = new List<ItemIndex>();
         private List<int> savedInventoryStacks = new List<int>();
+        private static ItemDef droneCompartmentItem;
 
         //The Awake() method is run at the very start when the game is initialized.
         public void Awake()
         {
             Log.Init(Logger);
 
+            droneCompartmentItem = ScriptableObject.CreateInstance<ItemDef>();
+
+            droneCompartmentItem.name = "DRONECOMPARTMENT_NAME";
+            droneCompartmentItem.nameToken = "DRONECOMPARTMENT_NAME";
+            droneCompartmentItem.pickupToken = "DRONECOMPARTMENT_PICKUP";
+            droneCompartmentItem.descriptionToken = "DRONECOMPARTMENT_DESC";
+            droneCompartmentItem.loreToken = "DRONECOMPARTMENT_LORE";
+
+#pragma warning disable Publicizer001
+            droneCompartmentItem._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/NoTierDef.asset").WaitForCompletion();
+#pragma warning restore Publicizer001
+
+            string bundleName = "dronereviveassets";
+            var assets = AssetBundle.LoadFromFile(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Info.Location), bundleName));
+            var chestIcon = assets.LoadAsset<Sprite>("Assets/chest.png");
+
+            droneCompartmentItem.pickupIconSprite = chestIcon;
+            droneCompartmentItem.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupMystery");
+
+            droneCompartmentItem.canRemove = false;
+            droneCompartmentItem.hidden = false;
+            var displayRules = new ItemDisplayRuleDict(null);
+
+            ItemAPI.Add(new CustomItem(droneCompartmentItem, displayRules));
+
             Log.Info(nameof(Awake) + " done.");
         }
 
-        private void GlobalEventManager_onCharacterDeathGlobal(RoR2.DamageReport report)
+        private void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
         {
             //If a character was killed by the world, we shouldn't do anything.
             if (!report.attacker || !report.attackerBody)
@@ -42,9 +68,6 @@ namespace ReturnsDroneRevive
                 return;
             }
             var attackerCharacterBody = report.attackerBody;
-
-            //attackerCharacterBody.inventory.RemoveItem();
-            //attackerCharacterBody.inventory.ResetItem();
 
             //We need an inventory to do check for our item
             if (attackerCharacterBody.inventory)
@@ -55,21 +78,21 @@ namespace ReturnsDroneRevive
 
         private void SpawnAsDrone() {
             Log.Info("Player pressed F2. Spawning as Drone");
-            RoR2.CharacterMaster master = playerInstance;
-            master.bodyPrefab = RoR2.BodyCatalog.GetBodyPrefab(RoR2.BodyCatalog.FindBodyIndex("Drone1Body"));
+            CharacterMaster master = playerInstance;
+            master.bodyPrefab = BodyCatalog.GetBodyPrefab(BodyCatalog.FindBodyIndex("Drone1Body"));
             ChangePlayerPrefab(master);
         }
 
         private void SpawnAsCharacter() {
             Log.Info("Player pressed F2. Spawning as Charracter");
-            RoR2.CharacterMaster master = playerInstance;
+            CharacterMaster master = playerInstance;
             master.bodyPrefab = playerSelectedCharacterBody;
             ChangePlayerPrefab(master);
         }
 
-        private void ChangePlayerPrefab(RoR2.CharacterMaster master) {
+        private void ChangePlayerPrefab(CharacterMaster master) {
             #pragma warning disable Publicizer001 // Accessing a member that was not originally public. Here we ignore this warning because with how this example is setup we are forced to do this
-                RoR2.ConVar.BoolConVar stage1pod = RoR2.Stage.stage1PodConVar;
+                RoR2.ConVar.BoolConVar stage1pod = Stage.stage1PodConVar;
                 bool oldVal = stage1pod.value;
                 stage1pod.SetBool(false);
                 var pcmc = master.playerCharacterMasterController;
@@ -87,17 +110,19 @@ namespace ReturnsDroneRevive
                 for(int j = 0; j < savedInventoryStacks[i]; j++) {
                     playerInstance.inventory.RemoveItem(playerInstance.inventory.itemAcquisitionOrder[i]);
                 }
-           }   
+            }   
+            playerInstance.inventory.GiveItem(droneCompartmentItem.itemIndex);           
         }
 
         private void AddBackInventory() {
+            playerInstance.inventory.RemoveItem(droneCompartmentItem.itemIndex);           
             for(int i = 0; i < savedInventory.Count; i++) {
                 for(int j = 0; j < savedInventoryStacks[i]; j++) {
                     playerInstance.inventory.GiveItem(savedInventory[i]);
                 }
                 savedInventory.Clear();
                 savedInventoryStacks.Clear();
-           } 
+            } 
         }
 
         //The Update() method is run on every frame of the game.
@@ -105,7 +130,7 @@ namespace ReturnsDroneRevive
         {
             if (Input.GetKeyDown(KeyCode.F2))
             {
-                playerInstance = RoR2.PlayerCharacterMasterController.instances[0].master;
+                playerInstance = PlayerCharacterMasterController.instances[0].master;
                 if(!isTransformed) {
                     playerSelectedCharacterBody = playerInstance.bodyPrefab;
                     SaveAndRemoveInventory();
@@ -119,24 +144,16 @@ namespace ReturnsDroneRevive
                 }
             }
             if (Input.GetKeyDown(KeyCode.F3)) {
-                playerInstance = RoR2.PlayerCharacterMasterController.instances[0].master;
+                playerInstance = PlayerCharacterMasterController.instances[0].master;
                 Log.Info("Giving Player Goathoof");
-                playerInstance.inventory.GiveItem(RoR2.ItemCatalog.FindItemIndex("Hoof"));
+                playerInstance.inventory.GiveItem(ItemCatalog.FindItemIndex("Hoof"));
             }
             if(Input.GetKeyDown(KeyCode.F4)) {
-                playerInstance = RoR2.PlayerCharacterMasterController.instances[0].master;
+                playerInstance = PlayerCharacterMasterController.instances[0].master;
                 Log.Info("Removing Goathoof From Player");
-                playerInstance.inventory.RemoveItem(RoR2.ItemCatalog.FindItemIndex("Hoof"));
-            }
-            if(Input.GetKeyDown(KeyCode.F5)) {
-                playerInstance = RoR2.PlayerCharacterMasterController.instances[0].master;
-                Log.Info("Changing inventory Color");
-                for(int i = 0; i < playerInstance.inventory.itemAcquisitionOrder.Count; i++) {
-                    SpriteRenderer changeColor = new SpriteRenderer();
-                    changeColor.sprite = RoR2.ItemCatalog.GetItemDef(playerInstance.inventory.itemAcquisitionOrder[i]).pickupIconSprite;
-                    changeColor.color = new Color(1,1,1,0.5f);
-                }
+                playerInstance.inventory.RemoveItem(ItemCatalog.FindItemIndex("Hoof"));
             }
         }
     }
+
 }
