705a3e4361a615940e2a5e8532619de0146cbf4b
natebouchat <natebouchat@gmail.com>
2024-02-29 21:56:40 -0600

    Drone respawn logic + bug fix

--- ReturnsDroneRevive/ReturnsDroneRevive.cs
+++ ReturnsDroneRevive/ReturnsDroneRevive.cs
@@ -1,16 +1,15 @@
 using System.Collections.Generic;
-using System.Reflection;
+using System.Collections;
 using BepInEx;
-using Mono.Security.Authenticode;
 using R2API;
 using RoR2;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.ResourceProviders;
+using IL.RoR2.ContentManagement;
 
 namespace ReturnsDroneRevive
 {
-    [BepInDependency(R2API.R2API.PluginGUID)]
+    [BepInDependency(ItemAPI.PluginGUID)]
+    [BepInDependency(LanguageAPI.PluginGUID)]
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
 
     public class ReturnsDroneRevive : BaseUnityPlugin
@@ -27,11 +26,50 @@ namespace ReturnsDroneRevive
         private List<int> savedInventoryStacks = new List<int>();
         private static ItemDef droneCompartmentItem;
 
-        //The Awake() method is run at the very start when the game is initialized.
         public void Awake()
         {
             Log.Init(Logger);
+            CreateItem();
 
+            Run.onRunStartGlobal += Run_onRunStartGlobal;
+            Stage.onServerStageBegin += Stage_onServerStageBegin;
+            GlobalEventManager.onCharacterDeathGlobal += GlobalEventManager_onCharacterDeathGlobal;
+            Log.Info(nameof(Awake) + " done.");
+        }
+
+        private void Run_onRunStartGlobal(Run obj) {
+            playerInstance = PlayerCharacterMasterController.instances[0].master;
+        }
+
+        private void Stage_onServerStageBegin(Stage obj) {
+            Log.Info($"New Stage: {nameof(Stage_onServerStageBegin)}");
+            Log.Info($"Prefab Body: {playerInstance.bodyPrefab.name}");
+            if(playerInstance.bodyPrefab.name.Equals("Drone1Body")) {
+                //pawnAsCharacter();
+            }
+        }
+
+        private void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
+        {
+            TrySpawnAsDrone();
+        }
+
+        private IEnumerator TrySpawnAsDrone() {
+            yield return new WaitForSeconds(1.5f);
+            int num;
+            if (playerInstance == null) {
+                num = 0;
+            }
+            else {
+                num = (((playerInstance != null) ? new bool?(playerInstance.IsDeadAndOutOfLivesServer()) : null) == false) ? 1 : 0;
+            }
+            if (num != 0 || Run.instance.isGameOverServer) {
+                yield break;
+            }
+            SpawnAsDrone();
+        }
+
+        private void CreateItem() {
             droneCompartmentItem = ScriptableObject.CreateInstance<ItemDef>();
 
             droneCompartmentItem.name = "DRONECOMPARTMENT_NAME";
@@ -40,89 +78,80 @@ namespace ReturnsDroneRevive
             droneCompartmentItem.descriptionToken = "DRONECOMPARTMENT_DESC";
             droneCompartmentItem.loreToken = "DRONECOMPARTMENT_LORE";
 
-#pragma warning disable Publicizer001
-            droneCompartmentItem._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/NoTierDef.asset").WaitForCompletion();
-#pragma warning restore Publicizer001
+            //NoTier
+            droneCompartmentItem.deprecatedTier = ItemTier.NoTier;
 
             string bundleName = "dronereviveassets";
             var assets = AssetBundle.LoadFromFile(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Info.Location), bundleName));
             var chestIcon = assets.LoadAsset<Sprite>("Assets/chest.png");
 
             droneCompartmentItem.pickupIconSprite = chestIcon;
-            droneCompartmentItem.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupMystery");
+            //droneCompartmentItem.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupMystery");
 
             droneCompartmentItem.canRemove = false;
             droneCompartmentItem.hidden = false;
             var displayRules = new ItemDisplayRuleDict(null);
 
             ItemAPI.Add(new CustomItem(droneCompartmentItem, displayRules));
-
-            Log.Info(nameof(Awake) + " done.");
-        }
-
-        private void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
-        {
-            //If a character was killed by the world, we shouldn't do anything.
-            if (!report.attacker || !report.attackerBody)
-            {
-                return;
-            }
-            var attackerCharacterBody = report.attackerBody;
-
-            //We need an inventory to do check for our item
-            if (attackerCharacterBody.inventory)
-            {
-                
-            }
         }
 
         private void SpawnAsDrone() {
             Log.Info("Player pressed F2. Spawning as Drone");
+            playerSelectedCharacterBody = playerInstance.bodyPrefab;
+            SaveAndRemoveInventory();
+
             CharacterMaster master = playerInstance;
             master.bodyPrefab = BodyCatalog.GetBodyPrefab(BodyCatalog.FindBodyIndex("Drone1Body"));
             ChangePlayerPrefab(master);
+            isTransformed = true;
         }
 
         private void SpawnAsCharacter() {
-            Log.Info("Player pressed F2. Spawning as Charracter");
+            Log.Info("Player pressed F2. Spawning as Character");
             CharacterMaster master = playerInstance;
             master.bodyPrefab = playerSelectedCharacterBody;
             ChangePlayerPrefab(master);
+
+            AddBackInventory();
+            isTransformed = false;
         }
 
         private void ChangePlayerPrefab(CharacterMaster master) {
             #pragma warning disable Publicizer001 // Accessing a member that was not originally public. Here we ignore this warning because with how this example is setup we are forced to do this
-                RoR2.ConVar.BoolConVar stage1pod = Stage.stage1PodConVar;
-                bool oldVal = stage1pod.value;
-                stage1pod.SetBool(false);
                 var pcmc = master.playerCharacterMasterController;
                 master.playerCharacterMasterController = null; // prevent metamorphosis rerolling the body for players
                 master.Respawn(master.GetBody().transform.position, master.GetBody().transform.rotation);
                 master.playerCharacterMasterController = pcmc;
-                stage1pod.SetBool(oldVal); 
             #pragma warning restore Publicizer001
         }
 
         private void SaveAndRemoveInventory() {
-           for(int i = 0; i < playerInstance.inventory.itemAcquisitionOrder.Count; i++) {
-                savedInventory.Add(playerInstance.inventory.itemAcquisitionOrder[i]);
+            int inventoryItemCount = playerInstance.inventory.itemAcquisitionOrder.Count;
+            for(int i = 0; i < inventoryItemCount; i++) {
+                Log.Info($"Inventory Count: {playerInstance.inventory.itemAcquisitionOrder.Count}");
+                for(int k = 0; k < playerInstance.inventory.itemAcquisitionOrder.Count; k++) {
+                    Log.Info($"Item: {ItemCatalog.GetItemDef(playerInstance.inventory.itemAcquisitionOrder[k]).name}");
+                }
+                savedInventory.Add(playerInstance.inventory.itemAcquisitionOrder[0]);
                 savedInventoryStacks.Add(playerInstance.inventory.GetItemCount(savedInventory[i]));
                 for(int j = 0; j < savedInventoryStacks[i]; j++) {
-                    playerInstance.inventory.RemoveItem(playerInstance.inventory.itemAcquisitionOrder[i]);
+                    playerInstance.inventory.RemoveItem(savedInventory[i]);
                 }
-            }   
+            }
+               
             playerInstance.inventory.GiveItem(droneCompartmentItem.itemIndex);           
         }
 
         private void AddBackInventory() {
-            playerInstance.inventory.RemoveItem(droneCompartmentItem.itemIndex);           
+            playerInstance.inventory.RemoveItem(droneCompartmentItem.itemIndex);
+
             for(int i = 0; i < savedInventory.Count; i++) {
                 for(int j = 0; j < savedInventoryStacks[i]; j++) {
                     playerInstance.inventory.GiveItem(savedInventory[i]);
                 }
-                savedInventory.Clear();
-                savedInventoryStacks.Clear();
-            } 
+            }
+            savedInventory.Clear();
+            savedInventoryStacks.Clear();
         }
 
         //The Update() method is run on every frame of the game.
@@ -130,26 +159,18 @@ namespace ReturnsDroneRevive
         {
             if (Input.GetKeyDown(KeyCode.F2))
             {
-                playerInstance = PlayerCharacterMasterController.instances[0].master;
                 if(!isTransformed) {
-                    playerSelectedCharacterBody = playerInstance.bodyPrefab;
-                    SaveAndRemoveInventory();
                     SpawnAsDrone();
-                    isTransformed = true;
                 }
                 else {
                     SpawnAsCharacter();
-                    AddBackInventory();
-                    isTransformed = false;
                 }
             }
             if (Input.GetKeyDown(KeyCode.F3)) {
-                playerInstance = PlayerCharacterMasterController.instances[0].master;
                 Log.Info("Giving Player Goathoof");
                 playerInstance.inventory.GiveItem(ItemCatalog.FindItemIndex("Hoof"));
             }
             if(Input.GetKeyDown(KeyCode.F4)) {
-                playerInstance = PlayerCharacterMasterController.instances[0].master;
                 Log.Info("Removing Goathoof From Player");
                 playerInstance.inventory.RemoveItem(ItemCatalog.FindItemIndex("Hoof"));
             }
