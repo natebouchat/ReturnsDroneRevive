4878e45afbc48a818a7de4ecffdbb51ac98a08fe
natebouchat <natebouchat@gmail.com>
2024-03-01 19:06:26 -0600

    Added Config File

--- ReturnsDroneRevive/ReturnsDroneRevive.cs
+++ ReturnsDroneRevive/ReturnsDroneRevive.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using BepInEx;
+using BepInEx.Configuration;
 using R2API;
 using RoR2;
 using UnityEngine;
@@ -21,10 +22,21 @@ namespace ReturnsDroneRevive
         private static GameObject playerSelectedCharacterBody;
         private static CharacterMaster playerInstance;
 
+        private static ConfigFile configFile;
+        private static ConfigEntry<bool> disableInventory;
+        private static KeyCode keybindForceSwap;
+        private static KeyCode keybindAscend;
+        private static KeyCode keybindDescend;
+
+
         public void Awake()
         {
             Log.Init(Logger);
-            InventoryManager.Init(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Info.Location), "dronereviveassets"));
+
+            SetFromConfig();
+            if(disableInventory.Value) {
+                InventoryManager.Init(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Info.Location), "dronereviveassets"));
+            }
 
             Run.onRunStartGlobal += Run_onRunStartGlobal;
             Stage.onServerStageBegin += Stage_onServerStageBegin;
@@ -32,10 +44,35 @@ namespace ReturnsDroneRevive
             Log.Info(nameof(Awake) + " done.");
         }
 
+        private void SetFromConfig() {
+            configFile = new ConfigFile(Paths.ConfigPath + "//ReturnsDroneRevive.cfg", true);
+            
+            disableInventory = configFile.Bind("DroneRevive", "Disable Inventory as Drone", true, "All collected items will be disabled as a drone. Items are returned next stage.");
+            ConfigEntry<string> keybindForceSwapString = configFile.Bind("DroneRevive", "Force Swap Keybind", "None", "Keybind for force swap transform to Drone/Character. Will also respawn if player is dead.");
+            ConfigEntry<string> keybindAscendString = configFile.Bind("DroneRevive", "Ascend as Drone Keybind", "Space", "Keybind to ascend as a drone.");
+            ConfigEntry<string> keybindDescendString = configFile.Bind("DroneRevive", "Descend as Drone Keybind", "LeftShift", "Keybind to descend as a drone");
+       
+            if(!System.Enum.TryParse(keybindForceSwapString.Value, out keybindForceSwap)) {
+                Log.Error("Failed Parse on \"Force Swap Keybind\". Defaulting to \"None\".");
+                keybindForceSwap = KeyCode.None;
+            }
+            if(!System.Enum.TryParse(keybindAscendString.Value, out keybindAscend)) {
+                Log.Error("Failed Parse on \"Ascend as Drone Keybind\". Defaulting to \"Space\".");
+                keybindAscend = KeyCode.Space;
+            }
+            if(!System.Enum.TryParse(keybindDescendString.Value, out keybindDescend)) {
+                Log.Error("Failed Parse on \"Descend as Drone Keybind\". Defaulting to \"LeftShift\".");
+                keybindDescend = KeyCode.LeftShift;
+            }
+        }
+
         private void Run_onRunStartGlobal(Run obj) {
             playerInstance = PlayerCharacterMasterController.instances[0].master;
-            InventoryManager.ClearSavedInventory();
-            playerInstance.inventory.onInventoryChanged += DroneInventoryChanged;
+            isTransformed = false;
+            if(disableInventory.Value) {
+                InventoryManager.ClearSavedInventory();
+                playerInstance.inventory.onInventoryChanged += DroneInventoryChanged;
+            }
         }
 
         private void Stage_onServerStageBegin(Stage obj) {
@@ -52,7 +89,7 @@ namespace ReturnsDroneRevive
         }
 
         private void DroneInventoryChanged() {
-            if(isTransformed && playerInstance.inventory.itemAcquisitionOrder.Count > 1) {
+            if(isTransformed && playerInstance.inventory.itemAcquisitionOrder.Count > 1 && disableInventory.Value) {
                 InventoryManager.StoreDroneCollectedItem(playerInstance);
             }
         }
@@ -75,7 +112,9 @@ namespace ReturnsDroneRevive
         private void SpawnAsDrone() {
             Log.Info("Spawning as Drone");
             playerSelectedCharacterBody = playerInstance.bodyPrefab;
-            InventoryManager.SaveAndRemoveInventory(playerInstance);
+            if(disableInventory.Value) {
+                InventoryManager.SaveAndRemoveInventory(playerInstance);
+            }
 
             CharacterMaster master = playerInstance;
             master.bodyPrefab = BodyCatalog.GetBodyPrefab(BodyCatalog.FindBodyIndex("Drone1Body"));
@@ -94,11 +133,13 @@ namespace ReturnsDroneRevive
                 playerInstance.bodyPrefab = playerSelectedCharacterBody;
             }
             isTransformed = false;
-            InventoryManager.AddBackInventory(playerInstance);
+            if(disableInventory.Value) {
+                InventoryManager.AddBackInventory(playerInstance);
+            }
         }
 
         private void ChangePlayerPrefab(CharacterMaster master) {
-            #pragma warning disable Publicizer001 // Accessing a member that was not originally public. Here we ignore this warning because with how this example is setup we are forced to do this
+            #pragma warning disable Publicizer001
                 var pcmc = master.playerCharacterMasterController;
                 master.playerCharacterMasterController = null; // prevent metamorphosis rerolling the body for players
                 master.Respawn(master.GetBody().transform.position, master.GetBody().transform.rotation);
@@ -109,7 +150,7 @@ namespace ReturnsDroneRevive
         //The Update() method is run on every frame of the game.
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.F2))
+            if (Input.GetKeyDown(keybindForceSwap))
             {
                 if(!isTransformed) {
                     SpawnAsDrone();
@@ -118,15 +159,15 @@ namespace ReturnsDroneRevive
                     SpawnAsCharacter(true);
                 }
             }
-            if (Input.GetKeyDown(KeyCode.F3)) {
-                Log.Info("Giving Player Goathoof");
-                playerInstance.inventory.GiveItem(ItemCatalog.FindItemIndex("Hoof"));
-            }
-            if(Input.GetKeyDown(KeyCode.F4)) {
-                Log.Info("Removing Goathoof From Player");
-                playerInstance.inventory.RemoveItem(ItemCatalog.FindItemIndex("Hoof"));
+            if(isTransformed) {
+                // Using Time.deltaTime causes jittery acsension
+                if (Input.GetKey(keybindAscend)) {
+                    playerInstance.GetBodyObject().transform.Translate(Vector3.up * 0.2f);
+                }
+                else if (Input.GetKey(keybindDescend)){
+                    playerInstance.GetBodyObject().transform.Translate(Vector3.down * 0.2f);
+                }
             }
         }
     }
-
 }
